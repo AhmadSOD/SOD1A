08f29c92eed10692fe168d49ef6254fffe7d42c4
AhmadSOD <144031243+AhmadSOD@users.noreply.github.com>
2024-03-08 13:45:23 +0100

    Add files via upload

---
+++ Project3/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+<footer >
+    <p>&copy; 2024 Top Scoot ... Alle rechten voorbehouden.</p>
+    <a href="toevoegen_review.php">Schrijv een review</a>
+    <a href="review.php">Reviews bekijken</a>
+    <a href="contact.php">Contact</a>
+</footer>
+</div>
+
